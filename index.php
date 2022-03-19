0a0957bc97cf252c6c4676b594ce8320994c8f7f
xMiCx <pratpong.work@gmail.com>
2022-03-19 15:37:23 +0700

    test

--- index.php
+++ index.php
@@ -110,7 +110,7 @@
         </div>
       </div>
     </section>
-    <!-- Copyright Section-->
+    
     <div class="copyright py-4 text-center text-white">
       <div class="container">
         <small>Copyright &copy; Your Website 2021</small>
